ad44848255f70f17c1ed0409a5ec0d3a04deb720
david ovidiu <eposta4work@gmail.com>
2014-12-14 01:03:01 +0100

    Initial commit

---
+++ WebAppAspNetMVC/WebAppAspNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppAspNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Is it you Snuggles?";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Here's how you can connect with Snuggles.";
+
+            return View();
+        }
+    }
+}
